5861061e1f1fde8cd5ba49cf005f59f494b2edf6
Henning <henninglee2013@gmail.com>
2016-04-13 00:00:05 +1000

    merge training data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,42 +1,37 @@
-getwd()
-## browsing the README file
+library(dplyr)
+##------1. Browsing dataset
+# README file
 readMe <- readLines("README.txt")
 readMe
-##feature info
+##browsing trainning data
+## get feature info
 featuresInfo <- readLines("features_info.txt")
 
-featuresInfo
 
-##get features
 
-features <- read.table("features.txt")
 
-features
+##------2. Merge training data
 
-list.files()
-## get labels
-actLables <- read.table("activity_labels.txt")
 
-##get trainning data
-list.files("./train")
-## subjective list
-subTrain <- read.table("./train/subject_train.txt")
-## extract x_train and y_train
+##merge y_train and activity_labels
+
+actLables <- read.table("activity_labels.txt")  ## get labels
+yTrain <- read.table("./train/y_train.txt")     ## extract y_train
+activity <- merge(yTrain, actLables,by = "V1", all = FALSE)$V2
+
+
+## combine activity and x_train
 xTrain <- read.table("./train/X_train.txt")
-yTrain <- read.table("./train/y_train.txt")
+features <- read.table("features.txt")
+newNames <- features[[2]]  ##change column name of xTrain
+colnames(xTrain) <- newNames
+xyTrain <- cbind(activity, xTrain)
 
+## merge xyTrain and subject
+subject <- read.table("./train/subject_train.txt")      ## subjective list
+subject <- rename(subject, subject = V1)        ##change the column name        
 
-##Merge features and xTrain 
-newNames <- as.character(features[[2]])
-
-oldNames <- xTrain[, c[1:561]]
-class(xTrain)
-mode(xTrain)
-
-
-
-
-
-
+##final dataset for training dataset
+xyTrain <- cbind(subject, xyTrain)
 
 
