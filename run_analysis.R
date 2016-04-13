5cc4148c371294ae1764982f300dc4c92ae37ca2
Henning <henninglee2013@gmail.com>
2016-04-13 16:51:34 +1000

    Final version- optimiz and fix errors

--- run_analysis.R
+++ run_analysis.R
@@ -14,82 +14,108 @@ list.files("./test")
 run.analysis <- function(dir) {
         cat("[run_analysis.R]","Author by Henning Li", "\n")
         cat("[run_analysis.R]","Created at 12-April-2016", "\n")
+        
         library(dplyr)
-        ##------1. Merge training data
-                cat("[run_analysis.R]","Choose the working data directory", "\n")
-                setwd(dir)
-                ##merge y_train and activity_labels AND Uses descriptive activity names to name the activities in the data set
+        library(reshape2)
+        cat("[run_analysis.R]","Choose the working data directory", "\n")
+        setwd(choose.dir())
+         
+        ##common lables with 6 obs. of 2 vars
+         actLables <- read.table("activity_labels.txt")
+         ##561 obs. of 2 vars
+         features <- read.table("features.txt")
+        
+        cat("[run_analysis.R]","merging train dataset...", "\n")
+        ##------1. reshape train data
+               
+               
+                ## train subject---extract train subject preparing for merging data
+                        ##with 7352 obs of 1 vars
+                        train.subject <- read.table("./train/subject_train.txt")      
+                        names(train.subject) <- "subject"   
                 
-                actLables <- read.table("activity_labels.txt")  ## get labels
-                yTrain <- read.table("./train/y_train.txt")     ## extract y_train
-                activity <- merge(yTrain, actLables,by = "V1", all = FALSE)$V2
+                ## train activity---merge y_train with activity_labels and get the train activity name vector
+                        ## with 7352 obs. of 1 vars
+                        train.y <- read.table("./train/y_train.txt")
+                        ## with 7652 activity obs. of 1 vars. As actLables is a factor, which need to be transfered to data frame
+                        train.activity <- data.frame(actLables[train.y[,1],2])
+                        names(train.activity) <- "activity"
+                   
+                ## train x --- change the name of  train activity with train.x
+                        ##with 7352 obs of 561 vars
+                        train.x <- read.table("./train/X_train.txt")
+                        ## change names of the column
+                        newNames <- features[[2]]  
+                        colnames(train.x) <- newNames
+                       
+                     
+                            
                 
+                ##combine train.subject, train.activity and train.x
+                        train.xy <- cbind(train.subject, train.activity, train.x)
+                        
+        cat("[run_analysis.R]","merging test dataset...", "\n")
+        ##------2. Merge test data 
+                        
+                ## train subject---extract train subject preparing for merging data
+                        ##with 2947 obs of 1 vars 
+                        test.subject<- read.table("./test/subject_test.txt") 
+                        test.subject<- rename(test.subject, subject = V1)
                 
-                ## combine activity and x_train
-                xTrain <- read.table("./train/X_train.txt")
-                features <- read.table("features.txt")
-                newNames <- features[[2]]  ##change column name of xTrain
-                colnames(xTrain) <- newNames
-                xyTrain <- cbind(activity, xTrain)
-                
-                ## merge xyTrain and subject
-                subject <- read.table("./train/subject_train.txt")      ## subjective list
-                subject <- rename(subject, subject = V1)        ##change the column name        
-                
-                ##final dataset for training dataset
-                xyTrain <- cbind(subject, xyTrain)
-                
+                ## Test activity --- merge y_train with activity_labels and get the train activity name vector
+                        ## with 2947 obs. of 1 vars
+                        test.y <- read.table("./test/y_test.txt")
+                        ## with 2947 activity obs. of 1 vars. As actLables is a factor, which need to be transfered to data frame
+                        test.activity <- data.frame(merge(test.y, actLables,by = "V1", all = FALSE)[,2])
+                        names(test.activity) <- "activity"
         
         
-        ##------2. Merge test data AND Uses descriptive activity names to name the activities in the data set
-                
-                actLables <- read.table("activity_labels.txt")  ## get labels
-                yTest <- read.table("./test/y_test.txt")     ## extract y_test
-                activity <- merge(yTest, actLables,by = "V1", all = FALSE)$V2
-                
-                
-                ## combine activity and x_test
-                xTest <- read.table("./test/X_test.txt")
-                features <- read.table("features.txt")
-                newNames <- features[[2]]  
-                colnames(xTest) <- newNames      ##change column name of xTest
-                xyTest <- cbind(activity, xTest)
-                
-                ## merge xyTest and subject
-                subjectTest<- read.table("./test/subject_test.txt")      ## subjective list
-                subjectTest<- rename(subjectTest, subject = V1)        ##change the column name        
-                
-                ##final dataset for test dataset
-                xyTest <- cbind(subjectTest, xyTest)
-                
-                
+                ## Test x --- change the name of  train activity with train.x
+                        ##with 2947 obs of 561 vars
+                        test.x <- read.table("./test/X_test.txt")
+                        ## change measures' name
+                        newNames <- features[[2]]  
+                        names(test.x) <- newNames    
+                        
+                          
+                        
+                ##combine test.subject, test.activity and test.x.
+                        test.xy <- cbind(test.subject,test.activity, test.x)
+        
+        cat("[run_analysis.R]","merging train and test dataset...", "\n")
+        
         ##------3. Merges the training and the test sets to create one data set.
-                data <-rbind(xyTrain, xyTest)
+                        
+                        data <-rbind(train.xy, test.xy)
         
+        cat("[run_analysis.R]","outputing dataset...", "\n")
         ##------4. Extracts only the measurements on the mean and standard deviation for each measurement.
         
-                x <- grep("[Mm]ean|[Ss]td|activity|subject", colnames(data))
-                data <- data[,x]
-                
+                        x <- grep("[Mm]ean|[Ss]td|activity|subject", colnames(data))
+                        data <- data[,x]
         
         
-        ##------5. create independent tidy data set with the average of each variable for each activity and each subject.
-                library(reshape2)
-                dataMelt <- melt(data, id = c("subject","activity"))    ##melt data in to id variables and measure variables
-                dataDcast <- dcast(dataMelt, subject + activity ~ variable, mean)
+        
+        ##------5. create independent tidy data set with the average of each variable for each activity and each subject
                 
+                        dataMelt <- melt(data, id = c("subject","activity"))    ##melt data in to id variables and measure variables
+                        dataDcast <- dcast(dataMelt, subject + activity ~ variable, mean)
+                        
+                        ##alternative method     
+                        ##library(plyr)
+                        ##averages <- ddply(data, .(subject, activity), function(x) colMeans(x[,3:88]))
+        
         ##------6. output the datasets
-                # Save the clean data.
-                cat("[run_analysis.R]","Choose directory to output CleanData.txt and dataDcast.txt", "\n")
-                setwd(choose.dir())
-                path <- file.path("cleanData.txt")
-                write.table(data, path, row.names = FALSE)
-                ##Save the aggregated datasets
-                path <- file.path("dataDcast.txt")
-                write.table(dataDcast, path, row.names = FALSE)
-                cat("[run_analysis.R]","output finish", "\n")
-
+                        # Save the clean data.
+                        cat("[run_analysis.R]","Choose directory to output CleanData.txt and dataDcast.txt", "\n")
+                        setwd(choose.dir())
+                        path <- file.path("cleanData.txt")
+                        write.table(data, path, row.names = FALSE)
+                        ##Save the aggregated datasets
+                        path <- file.path("dataDcast.txt")
+                        write.table(dataDcast, path, row.names = FALSE)
+                        cat("[run_analysis.R]","output finish", "\n")
+                        
 }
 ##run function
 run.analysis(choose.dir())
-
